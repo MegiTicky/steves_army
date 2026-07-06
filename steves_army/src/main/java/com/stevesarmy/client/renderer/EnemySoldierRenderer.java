559de34cc84c84688e6771db842e537658592be2
MegiRicky <lauyathong3518@gmail.com>
2026-07-06 17:01:26 +0800

    Fix enemy soldier crawl pose: match SoldierRenderer setupRotations
    - Add setupRotations override to EnemySoldierRenderer, same as SoldierRenderer
    - Uses getSwimAmount() to lerp body rotation for prone/crawl pose
    - Moves the model down and forward during rotation for correct visual position

--- steves_army/src/main/java/com/stevesarmy/client/renderer/EnemySoldierRenderer.java
+++ steves_army/src/main/java/com/stevesarmy/client/renderer/EnemySoldierRenderer.java
@@ -1,6 +1,6 @@
 package com.stevesarmy.client.renderer;
 
-import com.stevesarmy.client.model.SoldierModel;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.stevesarmy.entity.EnemySoldierEntity;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayers;
@@ -8,6 +8,8 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import org.joml.Quaternionf;
 
 public class EnemySoldierRenderer extends HumanoidMobRenderer<EnemySoldierEntity, HumanoidModel<EnemySoldierEntity>> {
     private static final ResourceLocation TEXTURE = new ResourceLocation("minecraft", "textures/entity/player/slim/steve.png");
@@ -23,5 +25,21 @@ public class EnemySoldierRenderer extends HumanoidMobRenderer<EnemySoldierEntity
     @Override
     public ResourceLocation getTextureLocation(EnemySoldierEntity entity) {
         return TEXTURE;
+    }
+
+    @Override
+    protected void setupRotations(EnemySoldierEntity soldier, PoseStack poseStack, float ageInTicks, float bodyYaw, float partialTick) {
+        float swim = soldier.getSwimAmount(partialTick);
+        if (swim > 0.001F && soldier.isAlive()) {
+            float rotX = Mth.lerp(swim, 0.0F, -90.0F);
+            double transY = Mth.lerp(swim, 0.0D, -1.0D);
+            double transZ = Mth.lerp(swim, 0.0D, 0.25D);
+
+            poseStack.mulPose(new Quaternionf().rotationY(180.0F - bodyYaw * Mth.DEG_TO_RAD));
+            poseStack.mulPose(new Quaternionf().rotationX(rotX * Mth.DEG_TO_RAD));
+            poseStack.translate(0.0D, transY, transZ);
+            return;
+        }
+        super.setupRotations(soldier, poseStack, ageInTicks, bodyYaw, partialTick);
     }
 }
